b2f10eea156476e9f0ddd9fd1adf2e811ebcca94
Himanshu Panwar <Himanshu_Panwar@Dell.com>
2022-05-26 20:51:02 +0530

    Add project files.

---
+++ AzureStepByStep/AzureCosmosDB/AzureCosmosDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
+namespace ConsoleApp7
+{
+    public class JsonData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    public class AzureCosmosDB
+    {
+        static void Main(string[] args)
+        {
+            const string EndpointUrl = @"https://mycosmos123.documents.azure.com:443/";
+            const string PrimaryKey = @"JV50wRKtAm7taXPh52zaiRgVBCR1sCNlMsnpV6b4Ie1iT1nU1gAw7hjeR3vpJPnBVzZUcZS4RkKSYuSpWOmjtg==";
+            DocumentClient client;
+            client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            IQueryable<JsonData> familyQuery = client.CreateDocumentQuery<JsonData>(
+                    UriFactory.CreateDocumentCollectionUri("db1", "coll1"), queryOptions);
+            foreach (var x in familyQuery)
+            {
+                Console.WriteLine("\tRead {0}", x.id + "  " + x.name);
+            }
+            Console.Read();
+        }
+    }
+}
